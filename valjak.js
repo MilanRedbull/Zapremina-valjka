a63deb1df6e8911abb966d80c420f19267251c93
MilanRedbull <94254089+MilanRedbull@users.noreply.github.com>
2022-01-13 13:39:44 +0100

    Add files via upload

---
+++ valjak.js
@@ -0,0 +1,7 @@
+var pocetak = alert ("Program za izracunavanje zapremine valjka");
+var h = prompt ("Unesite vrednost visine valjka:");
+var r = prompt ("Unesite poluprecnik valjka:");
+var r2= (r*r);
+var pi= 3.14;
+var v= (r2*h*pi);
+var v2= alert ("Zapremina valjka iznosi: " + v);
